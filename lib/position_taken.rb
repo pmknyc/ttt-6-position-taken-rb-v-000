306c8c07cf47d7567d847a68b355b3d5ad13c038
Paula McKinley <pmknyc@optimum.net>
2018-08-15 17:00:55 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+
+
+board = Array.new(9," ")
+index = 0
+
+def position_taken? (board, index)
+  #index = index.to_i
+  board[index] != " " && board[index] != "" && board[index] != nil
+
+
+end
