f0801e946e91809e6899dde255ea42813a0cebef
Paula McKinley <pmknyc@optimum.net>
2020-07-09 13:59:33 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,12 +1,30 @@
 # code your #position_taken? method here!
 
-
-board = Array.new(9," ")
+board=[" "," "," "," "," "," "," "," "," "]
 index = 0
 
 def position_taken? (board, index)
   #index = index.to_i
   board[index] != " " && board[index] != "" && board[index] != nil
-
-
 end
+
+=begin
+# Learn's solution
+def position_taken?(board, index)
+  # basic solution:
+
+  # taken = nil
+  # if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+  #   taken = false
+  # else
+  #   taken = true
+  # end
+  # taken
+
+  # advanced solution w/ ternary operator
+
+  (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+  false : true
+end
+
+=end
